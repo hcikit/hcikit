49c70635da727f6e39b34942f554a0f4cccfa832
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-05-28 16:15:33 -0400

    Made centered nice paper nicer

--- packages/tasks/src/components/index.js
+++ packages/tasks/src/components/index.js
@@ -1,28 +1,27 @@
-import styled from 'styled-components'
-import Paper from '@material-ui/core/Paper'
-import React from 'react'
-import propTypes from 'prop-types'
+import styled from "styled-components";
+import Paper from "@material-ui/core/Paper";
+import React from "react";
+import propTypes from "prop-types";
 
 export const NicePaper = styled(Paper)`
   max-width: 800px;
-  width: 100%;
   margin: 20px;
   padding: 20px;
-`
+`;
 
 export const FlexCenter = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
-  align-items: ${({ centerY }) => (centerY ? 'center' : 'flex-start')};
-  justify-content: ${({ centerX }) => (centerX ? 'center' : 'flex-start')};
-`
+  align-items: ${({ centerY }) => (centerY ? "center" : "flex-start")};
+  justify-content: ${({ centerX }) => (centerX ? "center" : "flex-start")};
+`;
 
 export const CenteredDiv = ({ children, centerX = true, centerY = false }) => (
   <FlexCenter centerX={centerX} centerY={centerY}>
     <div>{children}</div>
   </FlexCenter>
-)
+);
 
 CenteredDiv.propTypes = {
   children: propTypes.oneOfType([
@@ -31,7 +30,7 @@ CenteredDiv.propTypes = {
   ]).isRequired,
   centerX: propTypes.bool,
   centerY: propTypes.bool
-}
+};
 
 export const CenteredNicePaper = ({
   children,
@@ -41,7 +40,7 @@ export const CenteredNicePaper = ({
   <CenteredDiv centerX={centerX} centerY={centerY}>
     <NicePaper>{children}</NicePaper>
   </CenteredDiv>
-)
+);
 
 CenteredNicePaper.propTypes = {
   children: propTypes.oneOfType([
@@ -50,12 +49,12 @@ CenteredNicePaper.propTypes = {
   ]).isRequired,
   centerX: propTypes.bool,
   centerY: propTypes.bool
-}
+};
 
 export const CenteredText = styled.div`
   text-align: center;
-`
+`;
 
-export { LinearTimer } from './LinearTimer'
-export { ScreenFlash } from './ScreenFlash'
-export { default as SnackBar } from './Snackbar'
+export { LinearTimer } from "./LinearTimer";
+export { ScreenFlash } from "./ScreenFlash";
+export { default as SnackBar } from "./Snackbar";
