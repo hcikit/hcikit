b5053c07738e17e8aa9ac69b03dfed8f07173bf9
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-08-14 17:02:19 -0400

    Hopefully fixed travis build and added todo

--- example/src/App.test.js
+++ example/src/App.test.js
@@ -5,3 +5,5 @@ import App from "./App";
 it("renders without crashing", () => {
   shallow(<App />);
 });
+
+// TODO: the above is a good smoke test, but ideally we would also do more like making sure we can do an end to end run of an experiment and get the expected logs out...
