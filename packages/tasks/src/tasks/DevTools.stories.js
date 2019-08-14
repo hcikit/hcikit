bb09cc919f476fc9909e6d39d0223cbf01a195ae
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-08-14 16:04:10 -0400

    Move stories

---
+++ packages/tasks/src/tasks/DevTools.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import { DevTools } from "../src/tasks/DevTools";
+
+storiesOf("DevTools", module).add("example", () => (
+  <DevTools
+    configuration={{
+      children: [{ task: "Hello" }]
+    }}
+  />
+));
