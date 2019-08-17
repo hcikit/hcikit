a6001c074f3006791d9c97718068c0283d7ed0a0
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-08-16 20:11:03 -0400

    Added more tests

--- packages/tasks/src/tasks/DisplayTextTask.test.js
+++ packages/tasks/src/tasks/DisplayTextTask.test.js
@@ -1,7 +1,28 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import DisplayTextTask from "./DisplayTextTask";
+describe("DisplayTextTask", () => {
+  it("renders without crashing", () => {
+    mount(<DisplayTextTask content="hello" />);
+  });
 
-it("renders without crashing", () => {
-  shallow(<DisplayTextTask content="hello" />);
+  it("advances to the next task", () => {
+    let taskCompleteSpy = jest.fn();
+    let container = mount(
+      <DisplayTextTask taskComplete={taskCompleteSpy} content="Hello World" />
+    );
+
+    container.find("button").simulate("click");
+    expect(taskCompleteSpy).toBeCalledTimes(1);
+  });
+
+  // it("renders properly", () => {
+  //   expect(mount(<DisplayTextTask content="hello" />)).toMatchSnapshot();
+  // });
+
+  it("renders plain text", () => {
+    let container = mount(<DisplayTextTask content="hello" />);
+
+    expect(container.find("h1").text()).toBe("hello");
+  });
 });
