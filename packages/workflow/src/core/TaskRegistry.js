2018380b47e8ac329e29defc23e6179204aa6dcd
Blaine Lewis <blaine.lewis.1@outlook.com>
2019-09-27 23:21:53 -0400

    Tasks must start with a capital letter now

--- packages/workflow/src/core/TaskRegistry.js
+++ packages/workflow/src/core/TaskRegistry.js
@@ -7,13 +7,19 @@ export default class TaskRegistry {
 
   /**
  *
- * registerTask("MyTask", MyTask))
+ * registerTask("MyTask", MyTask)) 
+ * 
+ * The task must start with a capital letter.
  *
  * @param {string} taskName, the name of the component for lookup in configuration
  * @param {function} task, the component
 
  */
   registerTask = (taskName, task, reducer = undefined) => {
+    if (taskName[0].toUpperCase() !== taskName[0]) {
+      throw new Error("Task names must start with a capital letter.");
+    }
+
     this.tasks[taskName] = task;
 
     if (reducer) {
