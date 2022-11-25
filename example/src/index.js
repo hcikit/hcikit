5bd7573e07f549938dd15f9dcb61e16ccd26465d
Blaine Lewis <blaine.lewis.1@gmail.com>
2022-11-25 10:03:02 -0500

    Fixed issue with typescript createRoot

--- example/src/index.js
+++ example/src/index.js
@@ -5,5 +5,5 @@ import "./index.css";
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<App />);
